37dd38819cb51a9e8ee4d0d8d43a16036eda5407
VitorAlex-97 <vitualex97@gmail.com>
2021-11-22 12:32:21 -0300

    @VitorAlex-97 [task: fix #2 (repositories)]

--- src/main/java/com/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,12 +1,15 @@
 package com.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-
 	public Optional<Usuario> findByUsuario(String usuario);
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
